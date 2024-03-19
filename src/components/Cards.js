85bf319f1500375aea9274082d5195ae5643daf3
Mahn Bonnie <mulibonface187@gmail.com>
2024-03-19 11:40:00 +0300

    Updated components

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Fourteen Falls'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Mombasa in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-3.jpg'
+              text='Set Sail in the Indian Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-11.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-8.jpg'
+              text='Ride through the Chalbi Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
