9488581b4e50640c7df7c5aa286af5c6543852e0
Mahn Bonnie <mulibonface187@gmail.com>
2024-03-19 11:38:26 +0300

    Pages updated

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Products() {
-  return <h1 className='products'>SERVICES</h1>;
+  return <h1 className='products'>PRODUCTS</h1>;
 }
